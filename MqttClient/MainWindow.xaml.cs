0fd65fda9937eaeb6b00630730240dc88d804bda
OvenBryant <3063899248@qq.com>
2023-08-18 12:11:22 +0800

    frist commit

---
+++ MqttClient/MainWindow.xaml.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using System.Net;
+using System.Net.Sockets;
+using MQTTnet;
+using MQTTnet.Client;
+using NLE.Device.ZigBee;
+using System.Windows.Threading;
+
+namespace MqttClient
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        IMqttClient client;
+        ZigBeeSeries zig;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            zig = new ZigBeeSeries();
+            zig.DataReceived += Zig_DataReceived;
+
+            client = new MqttFactory().CreateMqttClient();
+            client.ApplicationMessageReceived += Client_ApplicationMessageReceived;
+            client.Connected += Client_Connected;
+            client.Disconnected += Client_Disconnected;
+            //textBox_IP.Text = Dns.GetHostAddresses(Dns.GetHostName())
+            //   .Where(t => t.AddressFamily.Equals(AddressFamily.InterNetwork))
+            //   .First()
+            //   .ToString();
+            textBox_IP.Text = "127.0.0.1";
+            textBox_Port.Text = "1883";
+
+            ChangeIsEndabled(false);
+        }
+
+        private void ChangeIsEndabled(bool b)
+        {
+            Dispatcher.Invoke(() =>
+            {
+                button_Connect.IsEnabled = !b;
+                button_DisConnect.IsEnabled = b;
+                button_Publish.IsEnabled = b;
+                button_Subscribe.IsEnabled = b;
+                button_Unsubscribe.IsEnabled = b;
+            });
+        }
+
+        private void Zig_DataReceived(object sender, ZigBeeDataEventArgs e)
+        {
+            message = new MqttApplicationMessage()
+            {
+                Topic = "temp",
+                Payload = Encoding.UTF8.GetBytes(e.Data.Value1.ToString())
+            };
+            client.PublishAsync(message);
+        }
+
+        private void Client_Disconnected(object sender, MqttClientDisconnectedEventArgs e)
+        {
+            MessageBox.Show("已断开");
+            ChangeIsEndabled(false);
+        }
+
+        private void Client_Connected(object sender, MqttClientConnectedEventArgs e)
+        {
+            MessageBox.Show("true");
+            zig.Connect("COM200", 38400);
+            ChangeIsEndabled(true);
+        }
+
+        private void Client_ApplicationMessageReceived(object sender, MqttApplicationMessageReceivedEventArgs e)
+        {
+            Dispatcher.Invoke(() =>
+            {
+                listBox_Record.Items.Add($"" +
+                    $"主题：{e.ApplicationMessage.Topic}" +
+                    $"内容：{Encoding.UTF8.GetString(e.ApplicationMessage.Payload)}");
+            });
+        }
+
+        IMqttClientOptions options;
+        private void button_Connect_Click(object sender, RoutedEventArgs e)
+        {
+            options = new MqttClientOptionsBuilder()
+                .WithTcpServer(textBox_IP.Text, int.Parse(textBox_Port.Text))
+                .Build();
+            client.ConnectAsync(options);
+        }
+
+        MqttApplicationMessage message;
+        private void button_Publish_Click(object sender, RoutedEventArgs e)
+        {
+            message = new MqttApplicationMessage()
+            {
+
+                Topic = textBox_Title.Text,
+                Payload = Encoding.UTF8.GetBytes(textBox_Content.Text)
+            };
+            client.PublishAsync(message);
+        }
+
+        private void button_Subscribe_Click(object sender, RoutedEventArgs e)
+        {
+            client.SubscribeAsync(textBox_Title.Text);
+        }
+
+        private void button_Unsubscribe_Click(object sender, RoutedEventArgs e)
+        {
+            client.UnsubscribeAsync(textBox_Title.Text);
+        }
+
+        private void button_DisConnect_Click(object sender, RoutedEventArgs e)
+        {
+            zig.Close();
+            client.DisconnectAsync();
+        }
+
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            listBox_Record.Items.Clear();
+        }
+    }
+}
---
+++ MqttServer/MainWindow.xaml.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using MQTTnet;
+using MQTTnet.Server;
+using System.Net;
+using System.Net.Sockets;
+
+namespace MqttServer
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        IMqttServer server;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            server = new MqttFactory().CreateMqttServer();
+            server.ClientConnected += Server_ClientConnected;  // 连接
+            server.ClientDisconnected += Server_ClientDisconnected;  // 断开
+            server.ClientSubscribedTopic += Server_ClientSubscribedTopic; // 订阅主题
+            server.ClientUnsubscribedTopic += Server_ClientUnsubscribedTopic;// 取消订阅主题
+            server.Started += Server_Started;
+            server.Stopped += Server_Stopped;
+
+
+            //textBox_IP.Text = Dns.GetHostAddresses(Dns.GetHostName())
+            //    .Where(t => t.AddressFamily.Equals(AddressFamily.InterNetwork))
+            //    .First()
+            //    .ToString();
+            textBox_IP.Text = "127.0.0.1";
+            // textBox_IP.Text = IPAddress.Any.ToString() ;
+            textBox_Port.Text = "1883";
+        }
+
+        private void Server_Stopped(object sender, EventArgs e)
+        {
+            Dispatcher.Invoke(() =>
+            {
+                listBox.Items.Add(string.Format("服务器已停止"));
+            });
+        }
+
+        private void Server_Started(object sender, EventArgs e)
+        {
+            Dispatcher.Invoke(() =>
+            {
+                listBox.Items.Add(string.Format("服务器已启动"));
+            });
+        }
+
+        private void Server_ClientUnsubscribedTopic(object sender, MqttClientUnsubscribedTopicEventArgs e)
+        {
+            Dispatcher.Invoke(() =>
+            {
+                listBox.Items.Add(string.Format("{0}已取消订阅{1}", e.ClientId, e.TopicFilter));
+            });
+        }
+
+        private void Server_ClientSubscribedTopic(object sender, MqttClientSubscribedTopicEventArgs e)
+        {
+            Dispatcher.Invoke(() =>
+            {
+                listBox.Items.Add(string.Format("{0}已订阅{1}", e.ClientId, e.TopicFilter.Topic));
+            });
+        }
+
+        private void Server_ClientDisconnected(object sender, MqttClientDisconnectedEventArgs e)
+        {
+            Dispatcher.Invoke(() =>
+            {
+                listBox.Items.Add(e.ClientId + "已断开");
+            });
+        }
+
+        private void Server_ClientConnected(object sender, MqttClientConnectedEventArgs e)
+        {
+            Dispatcher.Invoke(() =>
+            {
+                listBox.Items.Add(e.ClientId + "已连接");
+            });
+        }
+
+        bool isStart = false;
+        private void button_Switch_Click(object sender, RoutedEventArgs e)
+        {
+            isStart = !isStart;
+            if (isStart)
+            {
+                button_Switch.Content = "关闭";
+                IMqttServerOptions serverOptions = new MqttServerOptionsBuilder()
+                    .WithDefaultEndpointBoundIPAddress(IPAddress.Parse(textBox_IP.Text))
+                    .WithDefaultEndpointPort(int.Parse(textBox_Port.Text))
+                    .Build();
+                server.StartAsync(serverOptions);
+            }
+            else
+            {
+                button_Switch.Content = "开启";
+                server.StopAsync();
+            }
+            textBox_IP.IsEnabled = !isStart;
+            textBox_Port.IsEnabled = !isStart;
+        }
+    }
+}
